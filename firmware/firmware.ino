3596dd7607151ad611a381f0e841d756d3c38059
Satyam Khanna <satyamkhanna66@gmail.com>
2024-02-28 22:37:12 +0530

    Commit #2
    1. Fetching Geo Co-ords on MCU
    2. Added Script for routings between two geo co-ords

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,46 +1,6 @@
 #include <HardwareSerial.h>
-#include <config.h>
-
-void SystemClock_Config(void) {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {};
-
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 160;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
-} 
+#include <config_sys.h>
+#include <init_sys.h>
 
 String getMessageQuectel() {
   String str = "";
@@ -57,18 +17,35 @@ HardwareSerial Serial2(PA3, PA2);
 
 void setup() {
   SystemClock_Config();
-  
+
   DebugUart.begin(115200);
   DebugUart.println("SETUP");
-  
+
   QuectelUart.begin(115200);
+
+  pinMode(PA13, OUTPUT);
+  digitalWrite(PA13, HIGH);
+
+  delay(5000);
+
+  //DebugUart.println("AT+QGPSLOC=2");
+  //QuectelUart.println("AT+QGPSLOC=2");
+  //delay(100);
+  //DebugUart.println("AT+QGPSCFG=\"nmeasrc\",1");
+  //QuectelUart.println("AT+QGPSCFG=\"nmeasrc\",1");
+  //delay(100);
+  //DebugUart.println("AT+QGPSGNMEA=\"GGA\"");
+  //QuectelUart.println("AT+QGPSGNMEA=\"GGA\"");
+  //delay(100);
 }
 
 void loop() {
   String str;
-  QuectelUart.println("ATI");
+  DebugUart.println("AT+QGPSLOC=2");
+  QuectelUart.println("AT+QGPSLOC=2");
+  delay(100);
   str = getMessageQuectel();
-  if(str != ""){
+  if (str != "") {
     DebugUart.println(str);
   }
   delay(500);
