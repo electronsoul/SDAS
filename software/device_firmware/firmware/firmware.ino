d7ddedbab0cfa24738f26708ffd708c69d45a57d
Satyam Khanna <satyamkhanna66@gmail.com>
2024-04-21 22:28:47 +0530

    Commit
    To add school, hospital, ion based diaplay

--- software/device_firmware/firmware/firmware.ino
+++ software/device_firmware/firmware/firmware.ino
@@ -29,21 +29,23 @@ int myCompass = 0;
 double lat1, lon1, lat2, lon2;
 double d1 = 0, d2 = 0;
 
-byte fd_display[10]      = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x02};
-byte fd_display_R[10]    = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x03};
-byte lt_display[10]      = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x04};
-byte lt_display_R[10]    = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x05};
-byte rt_display[10]      = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x06};
-byte rt_display_R[10]    = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x07};
-byte fw_lt_display[10]   = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x08};
-byte fw_lt_display_R[10] = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x09};
-byte fw_rt_display[10]   = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x0A};
-byte fw_rt_display_R[10] = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x0B};
-byte round_display[10]   = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x0D};
-byte round_display_R[10] = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x0E};
-byte cross_display[10]   = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x0F};
-byte cross_display_R[10] = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x10};
-byte warning_display[10] = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x11};
+#define OOR_DISPLAY 16
+#define SPEED_LIMIT 40
+
+byte fd_display[]      = {0x5A, 0xA5, 0x05, 0x82, 0x10, 0x12, 0x00, 0x00};
+byte rt_display[]      = {0x5A, 0xA5, 0x05, 0x82, 0x10, 0x12, 0x00, 0x01};
+byte lt_display[]      = {0x5A, 0xA5, 0x05, 0x82, 0x10, 0x12, 0x00, 0x02};
+byte fw_rt_display[]   = {0x5A, 0xA5, 0x05, 0x82, 0x10, 0x12, 0x00, 0x03};
+byte fw_lt_display[]   = {0x5A, 0xA5, 0x05, 0x82, 0x10, 0x12, 0x00, 0x04};
+byte cross_display[]   = {0x5A, 0xA5, 0x05, 0x82, 0x10, 0x12, 0x00, 0x05};
+byte round_display[]   = {0x5A, 0xA5, 0x05, 0x82, 0x10, 0x12, 0x00, 0x06};
+byte hos_display[]     = {0x5A, 0xA5, 0x05, 0x82, 0x10, 0x12, 0x00, 0x07};
+byte sch_display[]     = {0x5A, 0xA5, 0x05, 0x82, 0x10, 0x12, 0x00, 0x08};
+
+byte ul_display[10] = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x02};
+byte ol_display[10] = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x03};
+
+byte out_of_range_display[10] = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, 0x00, 0x04};
 byte buz[] = {0x5A, 0xA5, 0x05, 0x82, 0x00, 0xA0, 0x00, 0x7D};
 
 double toRadians(double degrees) {
@@ -100,51 +102,80 @@ double calculateInitialCompassBearing(double lat1, double lon1, double lat2, dou
   return compass_bearing;
 }
 
-void setDisplay(int i) {
-  if (i == 1) {
-    DisplayUart.write(fd_display, sizeof(fd_display));
+void setDisplay(int i, int overlimit = 0) {
+
+  if (i == OOR_DISPLAY) {
+    DisplayUart.write(out_of_range_display, sizeof(out_of_range_display));
+    return;
   }
-  else if (i == 2) {
-    DisplayUart.write(fd_display_R, sizeof(fd_display_R));
+  if (overlimit == 1) {
+    DisplayUart.write(ol_display, sizeof(ol_display));
+    if (i == 0) {
+      fd_display[5] = 0x14;
+      DisplayUart.write(fd_display, sizeof(fd_display));
+    }
+    if (i == 1) {
+      rt_display[5] = 0x14;
+      DisplayUart.write(rt_display, sizeof(rt_display));
+    }
+    if (i == 2) {
+      lt_display[5] = 0x14;
+      DisplayUart.write(lt_display, sizeof(lt_display));
+    }
+    if (i == 3) {
+      fw_rt_display[5] = 0x14;
+      DisplayUart.write(fw_rt_display, sizeof(fw_rt_display));
+    }
+    if (i == 4) {
+      fw_lt_display[5] = 0x14;
+      DisplayUart.write(fw_lt_display, sizeof(fw_lt_display));
+    }
+    if (i == 5) {
+      cross_display[5] = 0x14;
+      DisplayUart.write(cross_display, sizeof(cross_display));
+    }
+    if (i == 6) {
+      round_display[5] = 0x14;
+      DisplayUart.write(round_display, sizeof(round_display));
+    }
+    if (i == 7) {
+      hos_display[5] = 0x14;
+      DisplayUart.write(hos_display, sizeof(hos_display));
+    }
+    if (i == 8) {
+      sch_display[5] = 0x14;
+      DisplayUart.write(sch_display, sizeof(sch_display));
+    }
   }
-  else if (i == 3) {
-    DisplayUart.write(lt_display, sizeof(lt_display));
-  }
-  else if (i == 4) {
-    DisplayUart.write(lt_display_R, sizeof(lt_display_R));
-  }
-  else if (i == 5) {
-    DisplayUart.write(rt_display, sizeof(rt_display));
-  }
-  else if (i == 6) {
-    DisplayUart.write(rt_display_R, sizeof(rt_display_R));
-  }
-  else if (i == 7) {
-    DisplayUart.write(fw_lt_display, sizeof(fw_lt_display));
-  }
-  else if (i == 8) {
-    DisplayUart.write(fw_lt_display_R, sizeof(fw_lt_display_R));
-  }
-  else if (i == 9) {
-    DisplayUart.write(fw_rt_display, sizeof(fw_rt_display));
-  }
-  else if (i == 10) {
-    DisplayUart.write(fw_rt_display_R, sizeof(fw_rt_display_R));
-  }
-  else if (i == 12) {
-    DisplayUart.write(round_display, sizeof(round_display));
-  }
-  else if (i == 13) {
-    DisplayUart.write(round_display_R, sizeof(round_display_R));
-  }
-  else if (i == 14) {
-    DisplayUart.write(cross_display, sizeof(cross_display));
-  }
-  else if (i == 15) {
-    DisplayUart.write(cross_display_R, sizeof(cross_display_R));
-  }
-  else if (i == 16) {
-    DisplayUart.write(warning_display, sizeof(warning_display));
+  else if (overlimit == 0) {
+    DisplayUart.write(ul_display, sizeof(ul_display));
+    if (i == 0) {
+      DisplayUart.write(fd_display, sizeof(fd_display));
+    }
+    if (i == 1) {
+      DisplayUart.write(rt_display, sizeof(rt_display));
+    }
+    if (i == 2) {
+      DisplayUart.write(lt_display, sizeof(lt_display));
+    }
+    if (i == 3) {
+      DisplayUart.write(fw_rt_display, sizeof(fw_rt_display));
+    }
+    if (i == 4) {
+      DisplayUart.write(fw_lt_display, sizeof(fw_lt_display));
+    }
+    if (i == 5) {
+      DisplayUart.write(cross_display, sizeof(cross_display));
+    }
+    if (i == 6) {
+      DisplayUart.write(round_display, sizeof(round_display));
+    }
+    if (i == 7) {
+      DisplayUart.write(hos_display, sizeof(hos_display));
+    }
+    if (i == 8) {
+      DisplayUart.write(sch_display, sizeof(sch_display));
+    }
   }
 
   //delay(10);
@@ -164,8 +195,10 @@ void extractDigits(int number, int &digit1, int &digit2, int &digit3) {
   digit3 = number % 10;           // Extract ones digit
 }
 
-int displayText(byte spd, int i) {
-  byte displaytext[] = {0x5A, 0xA5, 0x08, 0x82, 0x10, 0x20, 0x30, 0x30, 0x30, 0xFF, 0xFF};
+int displayText(byte spd, int i, int speedlimit = 0, int overlimit = 0) {
+  byte displaytext[]      = {0x5A, 0xA5, 0x08, 0x82, 0x11, 0x50, 0x30, 0x30, 0x30, 0xFF, 0xFF};
+  byte limitdisplaytext[] = {0x5A, 0xA5, 0x08, 0x82, 0x11, 0x60, 0x30, 0x30, 0x30, 0xFF, 0xFF};
+
   int digit1, digit2, digit3;
   extractDigits(spd, digit1, digit2, digit3);
 
@@ -173,112 +206,55 @@ int displayText(byte spd, int i) {
   digit2 = integerToAsciiByte(digit2);
   digit3 = integerToAsciiByte(digit3);
 
-  if (i == 1) {
-    displaytext[4] = 0x10; //d1
-    displaytext[5] = 0x20; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 2) {
-    displaytext[4] = 0x10; //d1
-    displaytext[5] = 0x40; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 3) {
-    displaytext[4] = 0x10; //d1
-    displaytext[5] = 0x60; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 4) {
-    displaytext[4] = 0x10; //d1
-    displaytext[5] = 0x80; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 5) {
+  int limit_digit1, limit_digit2, limit_digit3;
+  extractDigits(speedlimit, limit_digit1, limit_digit2, limit_digit3);
+
+  limit_digit1 = integerToAsciiByte(limit_digit1);
+  limit_digit2 = integerToAsciiByte(limit_digit2);
+  limit_digit3 = integerToAsciiByte(limit_digit3);
+
+  if (overlimit == 0) {
     displaytext[4] = 0x11; //d1
-    displaytext[5] = 0x00; //d1
+    displaytext[5] = 0x10; //d1
     displaytext[6] = digit1; //spd
     displaytext[7] = digit2; //spd
     displaytext[8] = digit3; //spd
-  }
-  else if (i == 6) {
+
+    limitdisplaytext[4] = 0x11; //d1
+    limitdisplaytext[5] = 0x20; //d1
+    limitdisplaytext[6] = limit_digit1; //spd
+    limitdisplaytext[7] = limit_digit2; //spd
+    limitdisplaytext[8] = limit_digit3; //spd
+  } else if (overlimit == 1) {
     displaytext[4] = 0x11; //d1
-    displaytext[5] = 0x20; //d1
+    displaytext[5] = 0x30; //d1
     displaytext[6] = digit1; //spd
     displaytext[7] = digit2; //spd
     displaytext[8] = digit3; //spd
+
+    limitdisplaytext[4] = 0x11; //d1
+    limitdisplaytext[5] = 0x40; //d1
+    limitdisplaytext[6] = limit_digit1; //spd
+    limitdisplaytext[7] = limit_digit2; //spd
+    limitdisplaytext[8] = limit_digit3; //spd
   }
-  else if (i == 7) {
+
+  if (i == OOR_DISPLAY) {
     displaytext[4] = 0x11; //d1
-    displaytext[5] = 0x40; //d1
+    displaytext[5] = 0x50; //d1
     displaytext[6] = digit1; //spd
     displaytext[7] = digit2; //spd
     displaytext[8] = digit3; //spd
-  }
-  else if (i == 8) {
-    displaytext[4] = 0x10; //d1
-    displaytext[5] = 0x60; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 9) {
-    displaytext[4] = 0x11; //d1
-    displaytext[5] = 0x80; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 10) {
-    displaytext[4] = 0x12; //d1
-    displaytext[5] = 0x00; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 12) {
-    displaytext[4] = 0x12; //d1
-    displaytext[5] = 0x40; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 13) {
-    displaytext[4] = 0x12; //d1
-    displaytext[5] = 0x60; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 14) {
-    displaytext[4] = 0x12; //d1
-    displaytext[5] = 0x80; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 15) {
-    displaytext[4] = 0x13; //d1
-    displaytext[5] = 0x00; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
-  }
-  else if (i == 16) {
-    displaytext[4] = 0x13; //d1
-    displaytext[5] = 0x20; //d1
-    displaytext[6] = digit1; //spd
-    displaytext[7] = digit2; //spd
-    displaytext[8] = digit3; //spd
+
+    limitdisplaytext[4] = 0x11; //d1
+    limitdisplaytext[5] = 0x60; //d1
+    limitdisplaytext[6] = limit_digit1; //spd
+    limitdisplaytext[7] = limit_digit2; //spd
+    limitdisplaytext[8] = limit_digit3; //spd
   }
   DisplayUart.write(displaytext, sizeof(displaytext));
+  delay(5);
+  DisplayUart.write(limitdisplaytext, sizeof(limitdisplaytext));
   //delay(50);
   return 1;
 }
@@ -394,14 +370,14 @@ void setup() {
       //DebugUart.println(str);
     }
   }
-  setDisplay(16);
-  displayText(0, 16);
+  setDisplay(OOR_DISPLAY);
+  displayText(0, OOR_DISPLAY, 40);
 
 }
 
 String str = "", latz = "" , lonz = "" , spdz = "", mapstr = "";
 float mycompass = 0, compprev = 0;
-int disSelect = 16;
+int disSelect = OOR_DISPLAY;
 int file_dir = 1, file_spd;
 int myspd = 0;
 int routeindex = 0;
@@ -419,14 +395,18 @@ void loop() {
     latz = latz.substring(0, 8);
     mylat = latz.toDouble();
     DebugUart.print(mylat, 6); DebugUart.print('\t');
+    //mylat = 0;
+
     lonz = str.substring(45);
     lonz = lonz.substring(0, 8);
     mylon = lonz.toDouble();
     DebugUart.print(mylon, 6); DebugUart.print('\t');
+    //mylon = 0;
+
     spdz = str.substring(73);
     spdz = spdz.substring(0, 3);
     myspd = spdz.toInt();
-    //DebugUart.print(myspd); DebugUart.print(" \t ");
+    DebugUart.print(myspd); DebugUart.print(" \t ");
 
     if (routeindex < route_size) {
       deserializeJson(jsonBuffer, String(routedata_sec85[routeindex]));
@@ -446,20 +426,28 @@ void loop() {
 
       if (isInRange(mylat, file_lat1, file_lat2) || isInRange(mylon, file_lon1, file_lon2))
       {
+        //todo
         DisplayUart.write(buz, sizeof(buz));
         disSelect = file_dir;
         routeindex++;
-        if (myspd > file_spd) {
-          disSelect++;
-        }
       }
-      setDisplay(disSelect);
-      displayText(myspd, disSelect);
 
+      if (myspd > file_spd) {
+        DisplayUart.write(buz, sizeof(buz));
+        setDisplay(disSelect, 1);
+        displayText(myspd, disSelect, file_spd, 1);
+      }
+      else {
+        setDisplay(disSelect, 0);
+        displayText(myspd, disSelect, file_spd, 0);
+      }
     }
     else {
-      setDisplay(16);
-      displayText(myspd, 16);
+      setDisplay(OOR_DISPLAY);
+      if (myspd > SPEED_LIMIT) {
+        DisplayUart.write(buz, sizeof(buz));
+      }
+      displayText(myspd, OOR_DISPLAY, SPEED_LIMIT);
     }
     DebugUart.print(routeindex); DebugUart.print('\t');
     DebugUart.println();
